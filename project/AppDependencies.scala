0ebfe248e37a19c4db16e568ea6a23cd054f698c
garytate123 <garytatelufc@gmail.com>
2024-01-24 14:54:04 +0000

    DDCE-5064 Remove Joda Time dep and upgrade other deps

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -10,7 +10,6 @@ object AppDependencies {
     "uk.gov.hmrc"       %% "play-frontend-hmrc"             % "7.29.0-play-28",
     "uk.gov.hmrc"       %% "play-conditional-form-mapping"  % "1.13.0-play-28",
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-28"     % bootstrapVersion,
-    "com.typesafe.play" %% "play-json-joda"                 % "2.9.4",
     "uk.gov.hmrc"       %% "domain"                         % "8.3.0-play-28"
   )
 
